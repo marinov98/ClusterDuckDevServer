97eef67da739994ddeff392e912be23c7a8fff77
Marin <Marin.Marinov38@myhunter.cuny.edu>
2020-01-25 14:46:04 -0500

    added google secret

--- src/utils/config/config.js
+++ src/utils/config/config.js
@@ -7,15 +7,17 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const port = process.env.PORT || 3002;
-const db_url = process.env.MONGODB_URL;
+const db_url = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
 const jwt_secret = process.env.JWT_SECRET || "DEFAULT_SECRET";
 const refresh_secret = process.env.JWT_REFRESH || "SECONDARY SECRET";
+const google_secret = process.env.GOOGLE_SECRET || "GOOGLE_SECRET";
 
 const configuration = {
   port,
   db_url,
   jwt_secret,
-  refresh_secret
+  refresh_secret,
+  google_secret
 };
 
 export default configuration;
