7d0f9d363cb1d66d36377909aa4a6a1df33d3fab
Marin <Marin.Marinov38@myhunter.cuny.edu>
2020-01-25 16:31:32 -0500

    added middleware for google auth

---
+++ src/middleware/authorize.js
@@ -0,0 +1,21 @@
+/**
+ * These middleware functions are used for people who have logged in with google auth
+ *
+ */
+
+/**
+ * @desc Function checks if a user has a token and is an admin
+ * @return Forbidden status if user either has no token or no admin header
+ */
+export function checkAdmin(req, res) {
+  if (!req.headers["Authorization"] || !req.headers["Admin"])
+    return res.sendStatus(403);
+}
+
+/**
+ * @desc Function that checks whether user ever logged in
+ * @return Forbidden status if user has no token
+ */
+export function authorize(req, res) {
+  if (!req.headers["Authorization"]) return res.sendStatus(403);
+}
