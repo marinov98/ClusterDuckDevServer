62fb5de6dbb4e4076538455083c24417da6a7268
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2020-01-27 23:28:18 -0500

    back to 15 min in refresh

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -134,7 +134,7 @@ router.post("/token", async (req, res, next) => {
     jwt.verify(refreshToken, config.refresh_secret, (err, user) => {
       if (err) return res.sendStatus(403);
       const accessToken = jwt.sign(user, config.jwt_secret, {
-        expiresIn: "15s"
+        expiresIn: "15m"
       });
       // send newly made token to user
       return res.status(200).json({ newToken: accessToken });
